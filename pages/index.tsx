3fdbc290d1610754264af9a697dc37305210b0cd
Jack Baron <jackmbaron@gmail.com>
2020-10-07 23:52:27 +0100

    basic UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
 import { NextPage } from 'next'
 import Head from 'next/head'
-import { Table } from '../components/Table'
+import { Selector } from '~components/Selector'
+import { Table } from '~components/Table'
 
 const App: NextPage = () => (
   <>
     <Head>
       <title>Phastracker</title>
     </Head>
+
     <div>
+      <Selector />
+      <hr />
       <Table />
     </div>
   </>
