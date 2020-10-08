734e2f64bfe8cd9a9732cf32b6feba4dd66a27b3
Jack Baron <jackmbaron@gmail.com>
2020-10-08 14:57:51 +0100

    style table

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,16 +1,49 @@
 import React, { FC } from 'react'
 import { Evidence } from '~data/evidence'
 import { IRow } from '~hooks/useData'
-import type { IStyledProps } from '~utils/styledProps'
 
-export const Row: FC<IRow & IStyledProps> = ({
-  type,
-  evidence,
-  className,
-  style,
-}) => (
-  <tr className={className} style={style}>
-    <td>{type}</td>
+export const Row: FC<IRow> = ({ type, evidence }) => (
+  <tr>
+    <style jsx>
+      {`
+        $border-radius = 0.5rem
+
+        td
+          &:first-child
+            border-left 0
+          &:last-child
+            border-right 0
+
+          border 1px solid rgb(80, 80, 80)
+          padding 0.35rem 0.6rem
+          text-align center
+          background-color rgba(255, 255, 255, 0.08)
+
+          &.ghost
+            text-align left
+            font-weight bold
+
+        tr:first-child
+          & > td
+            border-top 0
+          & > td:first-child
+            border-radius $border-radius 0 0 0
+          & > td:last-child
+            border-radius 0 $border-radius 0 0
+
+
+        tr:last-child
+          & > td
+            border-bottom 0
+          & > td:first-child
+            border-radius 0 0 0 $border-radius
+          & > td:last-child
+            border-radius 0 0 $border-radius 0
+
+      `}
+    </style>
+
+    <td className='ghost'>{type}</td>
 
     {Object.entries(Evidence).map(([key, ev]) => (
       <td key={key}>{evidence.includes(ev) ? ev : ''}</td>
--- components/Table.tsx
+++ components/Table.tsx
@@ -1,18 +1,34 @@
 import React, { FC } from 'react'
 import { useData } from '~hooks/useData'
-import type { IStyledProps } from '~utils/styledProps'
 import { Row } from './Row'
 
-export const Table: FC<IStyledProps> = ({ className, style }) => {
+export const Table: FC = () => {
   const { ghosts } = useData()
 
   return (
-    <table className={className} style={style}>
-      <tbody>
-        {ghosts.map(ghost => (
-          <Row key={ghost.type} {...ghost} />
-        ))}
-      </tbody>
-    </table>
+    <div className='container'>
+      <style jsx>
+        {`
+          .container
+            width 100%
+            border-radius 0.5rem
+            border 1px solid rgb(80, 80, 80)
+            margin-bottom 0.2rem
+            margin-top 1rem
+
+          table
+            width 100%
+            border-collapse collapse
+        `}
+      </style>
+
+      <table>
+        <tbody>
+          {ghosts.map(ghost => (
+            <Row key={ghost.type} {...ghost} />
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
