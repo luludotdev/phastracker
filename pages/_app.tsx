7340750590df90c612ef32bb05fd7abb5a0d6e81
Jack Baron <jackmbaron@gmail.com>
2020-10-08 14:11:45 +0100

    style main page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,33 @@ import { AppProps } from 'next/app'
 import { Provider } from '~components/Store'
 
 const NextApp = ({ Component, pageProps }: AppProps) => (
-  <Provider>
-    <Component {...pageProps} />
-  </Provider>
+  <>
+    <style jsx global>
+      {`
+        html, body
+          box-sizing border-box
+          background-color #111
+          color #fdfdfd
+          overflow-y hidden
+
+        *, *:before, *:after
+          box-sizing inherit
+
+        body
+          margin 0
+          padding 2rem 1rem
+          padding-bottom 0
+          font 16px/1.5 'Source Sans Pro', system-ui, sans-serif
+
+        ::selection
+          background-color rgba(255, 255, 255, 0.2)
+      `}
+    </style>
+
+    <Provider>
+      <Component {...pageProps} />
+    </Provider>
+  </>
 )
 
 export default NextApp
