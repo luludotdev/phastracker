e1a6ba41b2a95bf6ff3ad96f9decccf206167c81
Jack Baron <jackmbaron@gmail.com>
2020-10-09 13:33:07 +0100

    add profile component to display selected ghost

---
+++ components/Profile.tsx
@@ -0,0 +1,41 @@
+import { FC, useMemo } from 'react'
+import { ghosts, IGhost } from '~data/ghosts'
+import { useStore } from '~hooks/useStore'
+
+export const Profile: FC = () => {
+  const { state } = useStore()
+  const ghost = useMemo<IGhost>(() => {
+    const selected = ghosts.find(x => x.type === state.selected)
+    if (selected === undefined) throw new Error('Unknown ghost selected!')
+
+    return selected
+  }, [state.selected])
+
+  return (
+    <div>
+      <style jsx>
+        {`
+          h2
+            font-weight 500
+            margin-top 0.8rem
+            margin-bottom 0.2rem
+
+            &:first-child
+              margin-top 0
+
+          p
+            margin 0
+        `}
+      </style>
+
+      <h2>{ghost.type}</h2>
+      <p>{ghost.description}</p>
+
+      <h2>Strength</h2>
+      <p>{ghost.strength}</p>
+
+      <h2>Weakness</h2>
+      <p>{ghost.weakness}</p>
+    </div>
+  )
+}
