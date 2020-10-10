c4eead84cab159fdd0dc4f10d489200eacc77ac2
Jack Baron <jackmbaron@gmail.com>
2020-10-10 04:06:03 +0100

    wording

--- lib/data/evidence.ts
+++ lib/data/evidence.ts
@@ -2,7 +2,7 @@ export enum Evidence {
   SPIRIT_BOX = 'Spirit Box',
   FINGERPRINTS = 'Fingerprints',
   WRITING = 'Ghost Writing',
-  FREEZING = 'Freezing Temperature',
+  FREEZING = 'Freezing Temperatures',
   EMF_5 = 'EMF Level 5',
   ORBS = 'Ghost Orbs',
 }
