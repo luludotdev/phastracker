c9f04dec93a111b221af073272331dfdd9ed02a8
Jack Baron <jackmbaron@gmail.com>
2020-10-11 22:10:06 +0100

    media query hook

---
+++ lib/hooks/useMediaQuery.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react'
+
+export const useMediaQuery = (query: string) => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  return typeof window === 'undefined' ? false : useMediaQueryClient(query)
+}
+
+const useMediaQueryClient = (query: string) => {
+  const mql = window.matchMedia(query)
+  const [matches, setMatches] = useState(mql.matches)
+
+  useEffect(() => {
+    const listener = (event: MediaQueryListEvent) => setMatches(event.matches)
+    mql.addEventListener('change', listener)
+
+    return () => mql.removeEventListener('change', listener)
+  }, [mql])
+
+  return matches
+}
