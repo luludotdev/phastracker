1a229b5940e8370bb9df2dfcda873bc97d169a4c
Jack Baron <jackmbaron@gmail.com>
2021-06-28 13:44:20 +0100

    add/modify tips

--- lib/data/tips.ts
+++ lib/data/tips.ts
@@ -9,12 +9,19 @@ export enum Tip {
   HIDE = 'Hiding in a closet is often a more effective means of survival than running away.',
   BONE = 'Taking a photo of a bone before picking it up will net you more cash.',
   BONUS = 'If you find a bloody cleaver or a voodoo doll you can photograph them for bonus cash.',
-  CORPSE = "Snapping a photo of your buddies' corpse is worth a whole $10.",
-  PTT = 'The ghost can always hear you, even when not holding push-to-talk.',
+  CORPSE = "Snapping a photo of your buddies' corpse will give credit.",
+  PTT = 'The ghost cannot hear you if you are not holding push-to-talk, unless you are playing solo.',
   LOSE_ITEMS = "If you survive, all your items are returned to you. You don't have to bring them back to the truck.",
-  HUNT_SANITY = 'All ghost types start hunting when you drop below 50% sanity, with the exception of the Banshee which can hunt at any time.',
+  AVG_SANITY = 'Hunts are determined by *average* team sanity only.',
   HUNT_LIGHT = 'Turning your flashlight off during a hunt has no effect on the ghost tracking you.',
   CANDLE = 'Using candles for natural light can slow sanity drain.',
+  FUCK = 'Swearing out loud can anger the ghost.',
+  FIRST_NAME = "Saying just the ghost's first name is sufficient in catching its attention.",
+  SINKS = 'Turning on sinks has no effect on the chance of dirty water; it is best to let it happen naturally so you can hear it.',
+  SIGHT = 'Breaking line of sight is the most effective way to survive hunts.',
+  DOORS = 'Closing doors is essential for survival. It also helps in locating the ghost as you can track doors the ghost has opened.',
+  MIN_TEMP = 'Temperatures below 3°C always indicate Freezing Temperatures evidence.',
+  SPAWN_ROOM = 'The ghost will always spawn in the ghost room as soon as you unlock the door.',
 }
 
 export const randomTip: () => Tip = () => {
