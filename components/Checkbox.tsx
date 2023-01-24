9df4a811118e87dc6bb64b1ad026f4a1f6811af5
Jack Baron <jackmbaron@gmail.com>
2023-01-24 21:43:04 +0000

    update checkbox

--- components/Checkbox.tsx
+++ components/Checkbox.tsx
@@ -18,7 +18,7 @@ export const Checkbox: FC<Props> = ({ checked, disabled, onChange }) => {
   return (
     <input
       checked={checked}
-      className='form-checkbox cursor-pointer rounded !outline-none !ring-0 !ring-offset-0 disabled:cursor-default disabled:opacity-30 motion-safe:transition-all'
+      className='cursor-pointer rounded border-gray-300 text-blue-500 shadow-sm !outline-none !ring-0 !ring-offset-0 disabled:cursor-default disabled:opacity-30 motion-safe:transition-all'
       disabled={disabled}
       onChange={handleChange}
       type='checkbox'
